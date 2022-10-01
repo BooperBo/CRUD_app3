be917f351ce2c35c0ac4a8d345a919b73cea579d
dennz <dennzelw@gmail.com>
2022-10-01 17:04:03 +0500

    init

--- src/main/java/ru/springcourse/config/SpringConfig.java
+++ src/main/java/ru/springcourse/config/SpringConfig.java
@@ -58,7 +58,7 @@ public class SpringConfig implements WebMvcConfigurer {
 
         dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
         dataSource.setUrl(environment.getProperty("url"));
-        dataSource.setUsername(environment.getProperty("username"));
+        dataSource.setUsername(environment.getProperty("username_value"));
         dataSource.setPassword(environment.getProperty("password"));
 
         return dataSource;
